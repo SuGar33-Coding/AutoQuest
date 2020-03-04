6c946dff203cb6ff0dbf5e6abddec7de7c75a0db
Tanner Braun <tsbraun1891@gmail.com>
2020-03-04 14:42:30 -0500

    The first form of the basic update loop

---
+++ main.js
@@ -0,0 +1,54 @@
+
+var reset = false;
+var actionid;
+var go = false;
+
+function update() {
+    width = 0;
+    clearInterval(scene);
+    var player = { name: "Swaglord86x" };
+    actions = 0;
+    var element = document.getElementById("myprogressBar");
+    var width = 1;
+    var label = document.getElementById("actionLabel");
+    var tracker = document.getElementById("actionTracker");
+    // Calls scene every 50 ms
+    // So this resets every 5 seconds
+    var identity = setInterval(scene, 10);
+    function scene() {
+        if (go) {
+            label.innerHTML = actionid + "..."
+            if (reset) {
+                reset = false;
+                width = 0;
+                actions = 0;
+            }
+            if (width >= 100) {
+                width = 0;
+                actions++;
+                tracker.innerHTML = player.name + " has been " + actionid + " for " + actions + " actions!";
+            } else {
+                width++;
+                element.style.width = width + '%';
+            }
+        } else {
+            //clearInterval();
+            tracker.innerHTML = "";
+            label.innerHTML = "stopped."
+        }
+    }
+}
+
+function start(actionName) {
+    reset = true;
+    actionid = actionName;
+    if(go){
+
+    } else {
+        go = true;
+        //update();
+    }
+}
+
+update();
+
