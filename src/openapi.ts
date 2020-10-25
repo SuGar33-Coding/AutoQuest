41f11196fb3a1a16ad8c818aa877f3b22696e78b
Gabriel Klavans <gabeklav@bu.edu>
2020-10-25 15:25:22 -0400

    Get rid of "QUOTATIONS"

--- src/openapi.ts
+++ src/openapi.ts
@@ -1,47 +1,47 @@
 export default {
-    "openapi": "3.0.2",
-    "info": {
-        "title": "AutoQuest API",
-        "version": "1.0"
+    openapi: "3.0.2",
+    info: {
+        title: "AutoQuest API",
+        version: "1.0"
     },
-    "servers": [
-        { "url": "http://localhost:3000/" }
+    servers: [
+        { url: "http://localhost:3000/" }
     ],
-    "paths": {
+    paths: {
         "/fun/pog": {
-            "post": {
-                "parameters": [
+            post: {
+                parameters: [
                     {
-                        "name": "pogVal",
-                        "in": "query",
-                        "required": true,
-                        "schema": {
-                            "type": "integer"
+                        name: "pogVal",
+                        in: "query",
+                        required: true,
+                        schema: {
+                            type: "integer"
                         }
                     },
                     {
-                        "name": "pogName",
-                        "in": "query",
-                        "required": true,
-                        "schema": {
-                            "type": "string"
+                        name: "pogName",
+                        in: "query",
+                        required: true,
+                        schema: {
+                            type: "string"
                         }
                     },
                     {
-                        "name": "pogSecret",
-                        "in": "query",
-                        "required": false,
-                        "schema": {
-                            "type": "string"
+                        name: "pogSecret",
+                        in: "query",
+                        required: false,
+                        schema: {
+                            type: "string"
                         }
                     },
                 ],
-                "responses": {
-                    "200": {
-                        "description": "Nice"
+                responses: {
+                    200: {
+                        description: "Nice"
                     },
-                    "404": {
-                        "description": "Not found"
+                    404: {
+                        description: "Not found"
                     }
                 }
             }
